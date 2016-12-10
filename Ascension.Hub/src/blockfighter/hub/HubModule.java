ad10b705eb5c4b6a9e9fee09b36304dd5af6d07e
kenofnz <ken_kwan_master@hotmail.com>
2016-12-10 13:12:21 +1300

    Hub detects EC2 ip addresses correctly.

--- Ascension.Hub/src/blockfighter/hub/HubModule.java
+++ Ascension.Hub/src/blockfighter/hub/HubModule.java
@@ -1,18 +1,30 @@
 package blockfighter.hub;
 
 import blockfighter.hub.net.HubSender;
-import blockfighter.hub.net.HubServer;
 import blockfighter.shared.Globals;
 import blockfighter.shared.ServerInfo;
 import com.esotericsoftware.kryonet.Connection;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.apache.commons.net.util.SubnetUtils;
+import org.json.JSONObject;
 
 public class HubModule implements Runnable {
+
+    private static final List<JSONObject> EC2_IPS = new LinkedList<>();
+    private static final HashMap<String, SubnetUtils> IP_UTILS = new HashMap<>();
+    private static final HashMap<String, String> REGION_NAMES = new HashMap<>();
 
     private static final ConcurrentHashMap<Connection, ServerInfo> CONN_SERVERINFO_MAP = new ConcurrentHashMap<>();
     private final static ScheduledExecutorService GET_SERVERSTAT_SCHEDULER = Executors.newScheduledThreadPool(5, new BasicThreadFactory.Builder()
@@ -21,11 +33,52 @@ public class HubModule implements Runnable {
             .priority(Thread.NORM_PRIORITY)
             .build());
 
+    static {
+        long start = System.currentTimeMillis();
+        try {
+            URL ipURL = new URL("https://ip-ranges.amazonaws.com/ip-ranges.json");
+            JSONObject json = new JSONObject(IOUtils.toString(ipURL));
+            Iterator<Object> prefixes = json.getJSONArray("prefixes").iterator();
+            while (prefixes.hasNext()) {
+                JSONObject prefix = (JSONObject) prefixes.next();
+                if (prefix.getString("service").equalsIgnoreCase("ec2")) {
+                    EC2_IPS.add(prefix);
+                    IP_UTILS.put(prefix.getString("ip_prefix"), new SubnetUtils(prefix.getString("ip_prefix")));
+                }
+            }
+        } catch (IOException ex) {
+            Globals.logError(ex.toString(), ex, true);
+        }
+
+        REGION_NAMES.put("us-east-1", "N. Virginia, US East");
+        REGION_NAMES.put("us-east-2", "Ohio, US East");
+        REGION_NAMES.put("us-west-1", "N. California, US West");
+        REGION_NAMES.put("us-west-2", "Oregon, US West");
+        REGION_NAMES.put("eu-west-1", "Ireland");
+        REGION_NAMES.put("eu-central-1", "Frankfurt, Germany");
+        REGION_NAMES.put("ap-northeast-1", "Tokyo, Japan");
+        REGION_NAMES.put("ap-northeast-2", "Seoul, Korea");
+        REGION_NAMES.put("ap-southeast-1", "Singapore");
+        REGION_NAMES.put("ap-southeast-2", "Sydney, Australia");
+        REGION_NAMES.put("ap-south-1", "Mumbai, India");
+        REGION_NAMES.put("sa-east-1", "São Paulo, Brazil");
+        REGION_NAMES.put("ca-central-1", "Canada");
+        Globals.log(HubModule.class, (System.currentTimeMillis() - start) + "ms HubModule startup", Globals.LOG_TYPE_DATA, true);
+    }
+
     public static void addServerInfo(final Connection c, final ServerInfo info) {
         c.setKeepAliveTCP(1000);
         c.setTimeout(10000);
+        Iterator<JSONObject> ips = EC2_IPS.iterator();
+        while (ips.hasNext()) {
+            JSONObject ip = ips.next();
+            if (IP_UTILS.get(ip.getString("ip_prefix")).getInfo().isInRange(info.getAddress())) {
+                info.setRegion(REGION_NAMES.get(ip.getString("region")));
+                break;
+            }
+        }
         if (!CONN_SERVERINFO_MAP.containsKey(c)) {
-            Globals.log(HubServer.class, "Added " + info + " to server list", Globals.LOG_TYPE_DATA, true);
+            Globals.log(HubModule.class, "Added " + info + " to server list", Globals.LOG_TYPE_DATA, true);
         }
         CONN_SERVERINFO_MAP.put(c, info);
     }
@@ -38,7 +91,7 @@ public class HubModule implements Runnable {
     public static void removeServerInfo(final Connection c) {
         if (CONN_SERVERINFO_MAP.containsKey(c)) {
             ServerInfo info = CONN_SERVERINFO_MAP.remove(c);
-            Globals.log(HubServer.class, "Removed " + info + " from server list", Globals.LOG_TYPE_DATA, true);
+            Globals.log(HubModule.class, "Removed " + info + " from server list", Globals.LOG_TYPE_DATA, true);
         }
     }
 
