7a407953b38c78c98ad7cb0219346c34a8934e1e
kenofnz <ken_kwan_master@hotmail.com>
2017-02-17 19:59:42 +1300

    Updated end game board visuals

--- Ascension/src/blockfighter/client/screen/window/WindowMatchResult.java
+++ Ascension/src/blockfighter/client/screen/window/WindowMatchResult.java
@@ -9,6 +9,7 @@ import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Composite;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
@@ -35,10 +36,14 @@ public class WindowMatchResult extends Window {
     private double expGained = 0;
     private long countdownStartTime = 0;
     private int secondsLeftBeforeMenu = 0;
-    private float transparency = 0;
-    private Globals.VictoryStatus victoryStatus = Globals.VictoryStatus.LAST;
+    private float transparency = 0f;
 
-    private static final Rectangle2D.Double RESULT_BOARD = new Rectangle2D.Double(350, 200, 580, 250);
+    private Globals.VictoryStatus victoryStatus = Globals.VictoryStatus.LAST;
+    private static final Rectangle2D.Double RESULT_BOARD = new Rectangle2D.Double(350, 250, 580, 200);
+    private static final Polygon VICTORY_BG = new Polygon(
+            new int[]{(int) RESULT_BOARD.getMinX() + 50, (int) RESULT_BOARD.getMaxX() - 50, (int) RESULT_BOARD.getMaxX() - 20, (int) RESULT_BOARD.getMinX() + 20},
+            new int[]{(int) RESULT_BOARD.getMinY() - 50, (int) RESULT_BOARD.getMinY() - 50, (int) RESULT_BOARD.getMinY(), (int) RESULT_BOARD.getMinY()},
+            4);
     private static final Rectangle2D.Double RETURN_BUTTON = new Rectangle2D.Double(RESULT_BOARD.getCenterX() - 100, RESULT_BOARD.getMaxY() - 65, 200, 50);
 
     public WindowMatchResult(Screen parent) {
@@ -68,22 +73,15 @@ public class WindowMatchResult extends Window {
         try {
             g.setColor(BG_COLOUR);
             g.fill(RESULT_BOARD);
+            g.fill(VICTORY_BG);
             g.setColor(Color.BLACK);
             g.draw(RESULT_BOARD);
+            g.draw(VICTORY_BG);
 
             g.setColor(LEAVE_BUTTON_COLOUR);
             g.fill(RETURN_BUTTON);
             g.setColor(Color.BLACK);
             g.draw(RETURN_BUTTON);
-
-            g.setFont(Globals.ARIAL_18PTBOLD);
-
-            g.setColor(REWARDS_COLOUR);
-            g.drawString(MATCH_COMPLETION_REWARDS_TEXT, (int) (RESULT_BOARD.getMinX() + 15), (int) (RESULT_BOARD.getMinY() + 70));
-            g.drawString(victoryStatus.getBonusesText(), (int) (RESULT_BOARD.getCenterX() - g.getFontMetrics().stringWidth(victoryStatus.getBonusesText()) / 2), (int) (RESULT_BOARD.getMinY() + 95));
-
-            g.setColor(EXP_COLOUR);
-            g.drawString(TOTAL_EXP_GAINED_TEXT + Globals.NUMBER_FORMAT.format(this.expGained), (int) (RESULT_BOARD.getMinX() + 15), (int) (RESULT_BOARD.getMinY() + 130));
 
             switch (victoryStatus) {
                 case FIRST:
@@ -99,9 +97,19 @@ public class WindowMatchResult extends Window {
                     g.setColor(Color.WHITE);
                     break;
             }
-            g.setFont(Globals.ARIAL_24PTBOLD);
-            g.drawString(victoryStatus.toString(), (int) (RESULT_BOARD.getCenterX() - g.getFontMetrics().stringWidth(victoryStatus.toString()) / 2), (int) (RESULT_BOARD.getMinY() + 35));
+            g.setFont(Globals.MULAN_24PT.deriveFont(35f));
+            g.drawString(victoryStatus.toString(), (int) (VICTORY_BG.getBounds2D().getCenterX() - g.getFontMetrics().stringWidth(victoryStatus.toString()) / 2), (int) (VICTORY_BG.getBounds2D().getMinY() + 37));
 
+            g.setFont(Globals.ARIAL_18PTBOLD);
+            g.setColor(REWARDS_COLOUR);
+            g.drawString(MATCH_COMPLETION_REWARDS_TEXT, (int) (RESULT_BOARD.getMinX() + 15), (int) (RESULT_BOARD.getMinY() + 30));
+
+            g.drawString(victoryStatus.getBonusesText(), (int) (RESULT_BOARD.getCenterX() - g.getFontMetrics().stringWidth(victoryStatus.getBonusesText()) / 2), (int) (RESULT_BOARD.getMinY() + 55));
+
+            g.setColor(EXP_COLOUR);
+            g.drawString(TOTAL_EXP_GAINED_TEXT + Globals.NUMBER_FORMAT.format(this.expGained), (int) (RESULT_BOARD.getMinX() + 15), (int) (RESULT_BOARD.getMinY() + 90));
+
+            g.setFont(Globals.ARIAL_24PTBOLD);
             g.setColor(Color.WHITE);
             String returnText = RETURNING_TO_MENU_IN_TEXT + this.secondsLeftBeforeMenu + SECONDS_TEXT;
             g.drawString(returnText, (int) (RESULT_BOARD.getCenterX() - g.getFontMetrics().stringWidth(returnText) / 2), (int) (RETURN_BUTTON.getMinY() - 15));
@@ -165,7 +173,7 @@ public class WindowMatchResult extends Window {
     public void update() {
         this.secondsLeftBeforeMenu = 10 - (int) (Globals.nsToMs(Core.getLogicModule().getTime() - this.countdownStartTime) / 1000D);
         this.transparency = Globals.nsToMs(Core.getLogicModule().getTime() - this.countdownStartTime) / 1000f;
-        transparency = (transparency > 1) ? 1 : transparency;
+        this.transparency = (this.transparency > 1f) ? 1f : this.transparency;
+        this.transparency = (this.transparency < 0f) ? 0f : this.transparency;
     }
-
 }
