ddbc55900eb946c1862c84e520f2ae1c547d2a64
kenofnz <ken_kwan_master@hotmail.com>
2017-05-15 11:29:28 +1200

    Only query ip-api to fetch server info when info is null

--- Ascension/src/blockfighter/server/net/hub/HubSender.java
+++ Ascension/src/blockfighter/server/net/hub/HubSender.java
@@ -7,30 +7,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import org.apache.commons.io.IOUtils;
 
 public class HubSender {
 
-    public static void sendServerInfo() {
-        BufferedReader in;
+    private static String COUNTRY = null, AREA = null, PUBLIC_IP = null;
+
+    private static void fetchServerInfo() {
+        BufferedReader in = null;
         try {
             URL ipURL = new URL("http://checkip.amazonaws.com");
             in = new BufferedReader(new InputStreamReader(ipURL.openStream()));
-            String ip = in.readLine();
-
-            String country = "Unknown", area = "Unknown";
+            PUBLIC_IP = in.readLine();
             try {
-                URL regionURL = new URL("http://ip-api.com/line/" + ip + "?fields=country,regionName");
+                URL regionURL = new URL("http://ip-api.com/line/" + PUBLIC_IP + "?fields=country,regionName");
                 in = new BufferedReader(new InputStreamReader(regionURL.openStream()));
-                country = in.readLine();
-                area = in.readLine();
+                COUNTRY = in.readLine();
+                AREA = in.readLine();
             } catch (IOException ex) {
                 Globals.logError("Failed to get country name.", ex);
             }
-
-            ServerInfo info = new ServerInfo(ip, area + ", " + country, AscensionServer.getServerCapacityStatus());
-            HubClient.getClient().sendTCP(info);
         } catch (IOException ex) {
             Globals.logError(ex.toString(), ex);
+        } finally {
+            IOUtils.closeQuietly(in);
         }
+    }
+
+    public static void sendServerInfo() {
+        if (PUBLIC_IP == null || COUNTRY == null || AREA == null) {
+            fetchServerInfo();
+        }
+        ServerInfo info = new ServerInfo(PUBLIC_IP, COUNTRY + ", " + AREA, AscensionServer.getServerCapacityStatus());
+        HubClient.getClient().sendTCP(info);
     }
 }
