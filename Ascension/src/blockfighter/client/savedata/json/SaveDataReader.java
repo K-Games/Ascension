e4324807446fb2569ba38f587359bf3539c34ffb
Ken <ken_kwan_master@hotmail.com>
2018-11-07 11:07:44 +1300

    Encode save data

--- Ascension/src/blockfighter/client/savedata/json/SaveDataReader.java
+++ Ascension/src/blockfighter/client/savedata/json/SaveDataReader.java
@@ -3,14 +3,16 @@ package blockfighter.client.savedata.json;
 import blockfighter.client.savedata.SaveData;
 import blockfighter.shared.Globals;
 import java.util.Base64;
+import org.apache.commons.lang3.ArrayUtils;
 
 public final class SaveDataReader {
 
     public static SaveData readSaveData(final byte[] rawData) {
-        byte[] decodedBytes = Base64.getDecoder().decode(rawData);
+        byte[] firstPassDecode = Base64.getDecoder().decode(rawData);
+        ArrayUtils.reverse(firstPassDecode);
+        byte[] decodedBytes = Base64.getDecoder().decode(firstPassDecode);
         String reversedJson = new String(decodedBytes);
         String json = new StringBuffer(reversedJson).reverse().toString();
         return Globals.GSON.fromJson(json, SaveData.class);
     }
-
 }
--- Ascension/src/blockfighter/client/savedata/json/SaveDataWriter.java
+++ Ascension/src/blockfighter/client/savedata/json/SaveDataWriter.java
@@ -3,12 +3,15 @@ package blockfighter.client.savedata.json;
 import blockfighter.client.savedata.SaveData;
 import blockfighter.shared.Globals;
 import java.util.Base64;
+import org.apache.commons.lang3.ArrayUtils;
 
 public final class SaveDataWriter {
 
     public static byte[] writeSaveData(final SaveData c) {
         String json = Globals.GSON.toJson(c);
         String reversedJson = new StringBuffer(json).reverse().toString();
-        return Base64.getEncoder().encode(reversedJson.getBytes());
+        byte[] firstPassEncode = Base64.getEncoder().encode(reversedJson.getBytes());
+        ArrayUtils.reverse(firstPassEncode);
+        return Base64.getEncoder().encode(firstPassEncode);
     }
 }
